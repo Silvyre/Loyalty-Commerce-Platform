605c5b46055292ee6599b54b29a4641f416e5751
teddyhwang <tshwang@gmail.com>
2013-09-24 16:37:26 -0400

    udpate path for documentation.json file

--- static/scripts/modules/documentation.js
+++ static/scripts/modules/documentation.js
@@ -2,7 +2,7 @@
   var __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  define(['modules/process-documentation', 'jquery', 'json!/documents.json', 'hbars!../../templates/navigation', 'hbars!../../templates/section', 'hbars!../../templates/article'], function(ProcessDocumentation, $, documents, tmplNavigation, tmplSection, tmplArticle) {
+  define(['modules/process-documentation', 'jquery', 'json!/Voyager/documents.json', 'hbars!../../templates/navigation', 'hbars!../../templates/section', 'hbars!../../templates/article'], function(ProcessDocumentation, $, documents, tmplNavigation, tmplSection, tmplArticle) {
     var Documentation, _ref;
     Documentation = (function(_super) {
       __extends(Documentation, _super);
